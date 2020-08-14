8652c734fd78a56f10553902a4bbafe842e8145d
Juan Gallostra <juangallostra@gmail.com>
2020-08-14 09:40:50 +0200

    Update datafile with new sectors and zones

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json
 import urllib.request
+from datetime import date
 
 MAX_ITEMS_API_QUERY = 50
 Y_CRED = "AIzaSyAbPC02W3k-MFU7TmvYCSXfUPfH10jNB7g"
-
 
 def get_playlists(channel_id="UCX9ok0rHnvnENLSK7jdnXxA", num_playlists=MAX_ITEMS_API_QUERY):
     """
@@ -33,6 +33,28 @@ def get_playlists(channel_id="UCX9ok0rHnvnENLSK7jdnXxA", num_playlists=MAX_ITEMS
         zones += new_zones
         
     return [i['snippet']['title'] for i in zones]
+    
+def update_zones():
+    today = date.today()
+    # dd-mm-yyyy
+    current_date = today.strftime("%d-%m-%Y")
+    with open('zones.json', 'r', encoding='utf-8') as f:
+        current_data = json.load(f)
+    # get new zones and sectors
+    last_zone_update = current_data['zones']
+    current_zones = get_playlists()
+    new_zones = [zone for zone in current_zones if zone not in last_zone_update]
+    # prepare new data
+    updated_data = {
+        'date': current_date,
+        'zones': current_zones,
+        'new_zones': new_zones,
+        'previous_update': current_data['date']
+    }
+    with open('zones.json', 'w', encoding='utf-8') as f:
+        json.dump(updated_data, f, indent=4, ensure_ascii=False, sort_keys=True)
+    return new_zones
+
 
 if __name__ == "__main__":
-    print(get_playlists())
+    update_zones()
